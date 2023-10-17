36f5a81387420e3501e7e9a06fc31f585d28d927
widfleer <loelia.coutellier@gmail.com>
2023-10-17 12:21:02 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,12 +1,8 @@
-<div class="container">
-    <footer class="py-3 my-4">
-        <ul class="nav justify-content-center border-bottom pb-2 mb-3">
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
-        </ul>
-        <p class="text-center text-body-secondary">&copy; 2023 PimpMyPaids, Inc</p>
-    </footer>
-</div>
+<footer class="py-3 my-4 footer">
+    <ul class="nav justify-content-center border-bottom pb-2 mb-3">
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary"><i class="fa-solid fa-envelope"></i></a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Logo</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary"><i class="fa-solid fa-user-group"></i></a></li>
+    </ul>
+    <p class="text-center text-body-secondary">&copy; 2023 PimpMyPaids, Inc</p>
+</footer>
--- index.php
+++ index.php
@@ -1,48 +1,39 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>Page Title</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <!-- Nos pages CSS -->
-    <link
-      rel="stylesheet"
-      type="text/css"
-      media="screen"
-      href="assets/css/style.css"
-    />
-    <!-- FontAwesome -->
-    <link
-      href="assets/fonts/fontawesome/css/fontawesome.min.css"
-      rel="stylesheet"
-    />
-    <link href="assets/fonts/fontawesome/css/brands.min.css" rel="stylesheet" />
-    <link href="assets/fonts/fontawesome/css/solid.min.css" rel="stylesheet" />
-    <!-- Bootstrap -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-    <!-- Beer CSS -->
-    <link href="assets/css/beer.min.css" rel="stylesheet" />
-    <!-- JQuery -->
-  </head>
-  <body>
-    <i class="fa-solid fa-user"></i>
-    <i class="fa-brands fa-github-square"></i>
-    <i class="fa-solid fa-magnifying-glass fa-beat" style="color: #1f5120"></i>
-    <!-- Bootstrap -->
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <!-- JQuery -->
-    <script src="assets/js/jquery-3.7.1.min.js"></script>
-    <!-- Beer CSS -->
-    <script
-      type="module"
-      src="assets/js/beer.min.js"
-    ></script>
-    <script
-      type="module"
-      src="assets/js/material-dynamic-colors.min.js"
-    ></script>
-    <!-- HighCharts JS -->
-    <script src="assets/js/highcharts.js"></script>
-  </body>
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <title>Page Title</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <!-- Nos pages CSS -->
+  <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="assets/css/footer.css" />
+  <!-- FontAwesome -->
+  <link href="assets/fonts/fontawesome/css/fontawesome.min.css" rel="stylesheet" />
+  <link href="assets/fonts/fontawesome/css/brands.min.css" rel="stylesheet" />
+  <link href="assets/fonts/fontawesome/css/solid.min.css" rel="stylesheet" />
+  <!-- Bootstrap -->
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+  <!-- Beer CSS -->
+  <link href="assets/css/beer.min.css" rel="stylesheet" />
+  <!-- JQuery -->
+</head>
+
+<body>
+  <i class="fa-solid fa-user"></i>
+  <i class="fa-brands fa-github-square"></i>
+  <i class="fa-solid fa-magnifying-glass fa-beat" style="color: #1f5120"></i>
+  <?php include('./footer.php'); ?>
+  <!-- Bootstrap -->
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+  <!-- JQuery -->
+  <script src="assets/js/jquery-3.7.1.min.js"></script>
+  <!-- Beer CSS -->
+  <script type="module" src="assets/js/beer.min.js"></script>
+  <script type="module" src="assets/js/material-dynamic-colors.min.js"></script>
+  <!-- HighCharts JS -->
+  <script src="assets/js/highcharts.js"></script>
+</body>
+
 </html>
