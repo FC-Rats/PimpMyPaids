57dbfa9136765113c4cb436fc8c88c58ffb14088
Héléna Chevalier <lna.chevalier@gmail.com>
2023-10-22 17:54:17 +0200

    prototype header + modif bootstrap

---
+++ testheader.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <title>Page Title</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="icon" href="assets/img/logo.ico">
+  <!-- Nos pages CSS -->
+  <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="assets/css/header.css" />
+  <!-- FontAwesome -->
+  <link href="assets/fonts/fontawesome/css/fontawesome.min.css" rel="stylesheet" />
+  <link href="assets/fonts/fontawesome/css/brands.min.css" rel="stylesheet" />
+  <link href="assets/fonts/fontawesome/css/solid.min.css" rel="stylesheet" />
+  <!-- Bootstrap -->
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+  <!-- Beer CSS -->
+  <link href="assets/css/beer.min.css" rel="stylesheet" />
+</head>
+
+<body>
+    <?php include('./header.php'); ?>
+  <!-- Bootstrap -->
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+  <!-- JQuery -->
+  <script src="assets/js/jquery-3.7.1.min.js"></script>
+  <!-- Beer CSS -->
+  <script type="module" src="assets/js/beer.min.js"></script>
+  <script type="module" src="assets/js/material-dynamic-colors.min.js"></script>
+  <!-- HighCharts JS -->
+  <script src="assets/js/highcharts.js"></script>
+</body>
+
+</html>
