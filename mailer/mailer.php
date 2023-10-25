64f6cc51ab37156290c7df9df81307084d2876a3
Léo Dessertenne <leodesse2@gmail.com>
2023-10-25 21:10:11 +0200

    notation dromadaire

--- mailer/mailer.php
+++ mailer/mailer.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // include('./db-connection.php');
-include('./password_mail.php');
+include('./passwordMail.php');
 
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
