6d24527d8aeb205993cc0f79527cb95a0505a13b
widfleer <loelia.coutellier@gmail.com>
2023-11-14 21:53:20 +0100

    on a pas les droits ^^

--- footer.php
+++ footer.php
@@ -4,5 +4,5 @@
         <li class="nav-item"><a href="index.php?p=login" class="nav-link px-2 text-body-secondary" title="Accueil"><img class="p-1 px-3 img-fluid" src="assets/img/logorect.png" alt="Logo Pimp My Paids" title="Logo Pimp My Paids"/></a></li>
         <li class="nav-item"><a href="index.php?p=team" class="nav-link px-2 text-body-secondary" title="La team"><i class="fa-solid fa-user-group fa-xl"></i></a></li>
     </ul>
-    <p class="text-center text-body-secondary">&copy; <?php echo date('Y'); ?> PimpMyPaids. All Rights Reserved</p>
+    <p class="text-center text-body-secondary">&copy; <?php echo date('Y'); ?> PimpMyPaids</p>
 </footer>
