8b2eb0627eed63e35fbe53c6f81f05c40695882f
widfleer <loelia.coutellier@gmail.com>
2023-11-23 16:13:53 +0100

    clientSpace

--- assets/js/clientSpace.js
+++ assets/js/clientSpace.js
@@ -1,3 +1,21 @@
+// clientColorValueBalance
+var soldeElement = document.getElementById("clientBalance");
+var remittanceElement = document.getElementById("clientRemittances");
+var unpaidsElement = document.getElementById("clientUnpaids");
+var solde = parseFloat(soldeElement.textContent);
+var remittance = parseFloat(remittanceElement.textContent);
+var unpaids = parseFloat(unpaidsElement.textContent);
+if (solde < 0) {
+  soldeElement.style.color = "#AE2A00";
+}
+if (remittance < 0) {
+  remittanceElement.style.color = "#AE2A00";
+}
+if (unpaids < 0) {
+  unpaidsElement.style.color = "#AE2A00";
+}
+
+// clientBalanceChart
 Highcharts.chart("container-soldes", {
   chart: {
     type: "column",
--- clientSpace.php
+++ clientSpace.php
@@ -74,8 +74,7 @@
 
 
     <?php include('./footer.php'); ?>
-    <script src="assets/js/clientColorValueBalance.js"></script>
-    <script src="assets/js/clientBalanceChart.js"></script>
+    <script src="assets/js/clientSpace.js"></script>
     <!-- JQuery -->
     <script src="assets/js/jquery-3.7.1.min.js"></script>
     <!-- Bootstrap -->
