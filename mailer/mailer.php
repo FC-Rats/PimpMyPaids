456dc31c7b23aa2cf3ffad2300b14dd864fd678c
Hell <lna.chevalier@gmail.com>
2023-12-04 16:17:49 +0100

    fix mailer path

--- mailer/mailer.php
+++ mailer/mailer.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // include('./db-connection.php');
-$config = parse_ini_file('config.ini');
+$config = parse_ini_file('./includes/config.ini');
 
 require '../framework/PHPMailer/src/Exception.php';
 require '../framework/PHPMailer/src/PHPMailer.php';
