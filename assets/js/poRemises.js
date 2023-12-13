dc6cb6ec3813c9a5b63c7f37707817aa18bb8be8
widfleer <loelia.coutellier>
2023-12-13 09:39:48 +0100

    export data Detail poRemises

--- assets/js/poRemises.js
+++ assets/js/poRemises.js
@@ -27,11 +27,11 @@ $(function () {
 
     // pagination
     $("#nbLineByPage").on("input", function () {
-        rowsPerPage=$(this).val();
+        rowsPerPage = $(this).val();
         printRemittances();
     });
     $("#pageToShow").on("input", function () {
-        pageToShow=$(this).val();
+        pageToShow = $(this).val();
         printRemittances();
     });
 
@@ -44,25 +44,38 @@ $(function () {
         afterDate = $('#afterDate').val();
         context = $('#context').val();
         export_type = $('#export_type').val();
-/*         var dataToExport = {
-            "siren": siren,
-            "companyName": companyName,
-            "remittanceNumber": remittanceNumber,
-            "beforeDate": beforeDate,
-            "afterDate": afterDate,
-            "context": context,
-            "export_type": export_type,
-            "rows": listRemittancesData
-        }; */
         dataToExport = JSON.stringify(listRemittancesData);
         $.ajax({
             url: "export/export_data.php",
             type: "POST",
             dataType: "JSON",
-            //data: { "dataToExport": dataToExport },
             data: { "siren": siren, "companyName": companyName, "remittanceNumber": remittanceNumber, "beforeDate": beforeDate, "afterDate": afterDate, "context": context, "export_type": export_type, "dataToExport": dataToExport },
             success: function (data) {
                 window.location.href = data.fileUrl; // Cela déclenchera le téléchargement du fichier
+                $("#export_type").val("noExport");
+            },
+            error: function (data) {
+                console.log(data);
+            },
+        });
+    });
+
+    // export details
+    $("#export_typeDetails").on("change", function () {
+        var idDetails = $('#idDetails').val();
+        companyName = $('#offcanvasDetailRemittancePo #companyNameDetail').val();
+        remittanceNumber = $('#offcanvasDetailRemittancePo #idRemittanceDetail').val();
+        context = $('#contextDetails').val();
+        export_type = $('#export_typeDetails').val();
+        dataToExport = JSON.stringify(listRemittancesData[idDetails].details);
+        $.ajax({
+            url: "export/export_data.php",
+            type: "POST",
+            dataType: "JSON",
+            data: { "companyName": companyName, "remittanceNumber": remittanceNumber, "beforeDate": beforeDate, "afterDate": afterDate, "context": context, "export_type": export_type, "dataToExport": dataToExport },
+            success: function (data) {
+                window.location.href = data.fileUrl; // Cela déclenchera le téléchargement du fichier
+                $("#export_typeDetails").val("noExport");
             },
             error: function (data) {
                 console.log(data);
@@ -93,7 +106,7 @@ function listRemittances(
             $.map(listRemittancesData, function (data, dataKey) {
                 remittanceNumberToKey[data.remittanceNumber] = dataKey;
             });
-            
+
             $("#pageToShow").val(1);
             $("#nbLineByPage").val(Math.min(3, listRemittancesData.length));
             rowsPerPage = $('#nbLineByPage').val();
@@ -109,19 +122,19 @@ function listRemittances(
 
 function printRemittances() {
     $("#container-remise-list").empty();
-    var start = rowsPerPage*(pageToShow-1);
-    var sum = parseInt(start, 10)+parseInt(rowsPerPage, 10);
+    var start = rowsPerPage * (pageToShow - 1);
+    var sum = parseInt(start, 10) + parseInt(rowsPerPage, 10);
     var itemsToShow = listRemittancesData.slice(start, sum);
     $.map(itemsToShow, function (data, dataKey) {
         var html = "";
-        html += '<div class="remise-element rounded-3 my-3 px-2 px-lg-5 py-3 d-flex flex-row flex-wrap justify-content-end justify-content-lg-between align-items-center" id="'+ data.remittanceNumber +'">';
-        html += '<span class="col-12 col-sm-6 col-lg-1 siren">'+ data.siren +'</span>';
-        html += '<span class="col-12 col-sm-6 col-lg-2 companyName">'+ data.companyName +'</span>';
-        html += '<span class="col-12 col-sm-6 col-lg-1 remittanceNumber">'+ data.remittanceNumber +'</span>';
-        html += '<span class="col-12 col-sm-6 col-lg-1 dateRemittance">'+ data.dateRemittance +'</span>';
-        html += '<span class="col-12 col-sm-6 col-lg-2 nbTransactions">'+ data.nbTransactions +' transaction(s)</span>';
-        html += '<span class="col-12 col-sm-6 col-lg-2 totalAmount">'+ data.montantTotal +' ('+ data.currency +')</span>';
-        html += '<button class="col-12 col-sm-6 col-lg-1 d-flex justify-content-end clear-button fs-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDetailRemittancePo" aria-controls="offcanvasDetailRemittancePo" onclick="viewDetailRemittance('+ remittanceNumberToKey[data.remittanceNumber] +');">+</button>';
+        html += '<div class="remise-element rounded-3 my-3 px-2 px-lg-5 py-3 d-flex flex-row flex-wrap justify-content-end justify-content-lg-between align-items-center" id="' + data.remittanceNumber + '">';
+        html += '<span class="col-12 col-sm-6 col-lg-1 siren">' + data.siren + '</span>';
+        html += '<span class="col-12 col-sm-6 col-lg-2 companyName">' + data.companyName + '</span>';
+        html += '<span class="col-12 col-sm-6 col-lg-1 remittanceNumber">' + data.remittanceNumber + '</span>';
+        html += '<span class="col-12 col-sm-6 col-lg-1 dateRemittance">' + data.dateRemittance + '</span>';
+        html += '<span class="col-12 col-sm-6 col-lg-2 nbTransactions">' + data.nbTransactions + ' transaction(s)</span>';
+        html += '<span class="col-12 col-sm-6 col-lg-2 totalAmount">' + data.montantTotal + ' (' + data.currency + ')</span>';
+        html += '<button class="col-12 col-sm-6 col-lg-1 d-flex justify-content-end clear-button fs-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDetailRemittancePo" aria-controls="offcanvasDetailRemittancePo" onclick="viewDetailRemittance(' + remittanceNumberToKey[data.remittanceNumber] + ');">+</button>';
         html += '</div>';
 
         $("#container-remise-list").append(html);
@@ -136,8 +149,8 @@ function formatDate() {
         var dateRemittance = $(this).text();
         var date = new Date(dateRemittance);
         var formattedDate = ("0" + date.getDate()).slice(-2) + "/"
-                             + ("0" + (date.getMonth() + 1)).slice(-2) + "/"
-                             + date.getFullYear();
+            + ("0" + (date.getMonth() + 1)).slice(-2) + "/"
+            + date.getFullYear();
         $(this).text(formattedDate);
     });
 }
@@ -151,14 +164,22 @@ function viewDetailRemittance(id) {
 
     $.map(dataDetail.details, function (data, dataKey) {
         html = "";
-        html += '<div class="remise-element rounded-3 my-3 px-2 py-3 d-flex flex-row flex-wrap justify-content-between align-items-center" id="'+ data.numAutorisation +'">';
-        html += '<span class="col-12 dateTransac">'+ data.dateTransac +'</span>';
-        html += '<span class="col-4 network">'+ data.network +'</span>';
-        html += '<span class="col-6 creditCardNumber">'+ data.creditCardNumber +'</span>';
-        html += '<span class="col-12 numAutorisation">'+ data.numAutorisation +'</span>';
-        html += '<span class="col-1 sign">'+ data.sign +'</span>';
-        html += '<span class="col-4 amount">'+ data.amount +'</span>';
-        html += '<span class="col-2 currency">'+ data.currency +'</span>';
+        html += '<input type="hidden" name="contextDetails" value="poRemisesDetails" id="contextDetails">';
+        html += '<input type="hidden" name="idDetails" value="'+id+'" id="idDetails">';
+        html += '<select class=" d-flex form-select btn btn-primary border-0 p-1 pe-5" name="export_typeDetails" id="export_typeDetails"></select>';
+        html += '<option value="noExport" selected>Exporter les données</option>';
+        html += '<option value="pdf">PDF</option>';
+        html += '<option value="csv">CSV</option>';
+        html += '<option value="xls">XLS</option>';
+        html += '</select>';
+        html += '<div class="remise-element rounded-3 my-3 px-2 py-3 d-flex flex-row flex-wrap justify-content-between align-items-center" id="' + data.numAutorisation + '">';
+        html += '<span class="col-12 dateTransac">' + data.dateTransac + '</span>';
+        html += '<span class="col-4 network">' + data.network + '</span>';
+        html += '<span class="col-6 creditCardNumber">' + data.creditCardNumber + '</span>';
+        html += '<span class="col-12 numAutorisation">' + data.numAutorisation + '</span>';
+        html += '<span class="col-1 sign">' + data.sign + '</span>';
+        html += '<span class="col-4 amount">' + data.amount + '</span>';
+        html += '<span class="col-2 currency">' + data.currency + '</span>';
         html += '</div>';
 
         $("#offcanvasDetailRemittancePo #offcanvas-body").append(html);
@@ -173,8 +194,8 @@ function formatDateDetail() {
         var dateTransac = $(this).text();
         var date = new Date(dateTransac);
         var formattedDate = ("0" + date.getDate()).slice(-2) + "/"
-                             + ("0" + (date.getMonth() + 1)).slice(-2) + "/"
-                             + date.getFullYear();
+            + ("0" + (date.getMonth() + 1)).slice(-2) + "/"
+            + date.getFullYear();
         $(this).text(formattedDate);
     });
 }
